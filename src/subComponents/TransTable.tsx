3ec462b7f47a1f18a1e66770536ef1db31454ee0
ariyanmvn <ariyan@moveon.global>
2025-03-12 08:58:54 +0600

    fixed table icons for mobile screens

--- src/subComponents/TransTable.tsx
+++ src/subComponents/TransTable.tsx
@@ -8,20 +8,38 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import SearchIcon from "@/icons/SearchIcon";
 import { Input } from "@/components/ui/input";
 import SearchBlackIcon from "@/icons/SearchBlackIcon";
+import FilterIcon from "@/icons/FilterIcon";
+import ExportIcon from "@/icons/ExportIcon";
 
 export default function TransTable() {
   return (
-    <div className="bg-[#FFFFFF] rounded-[16px] xl:mt-5 w-[100%] xl-w-full h-[900px] mx-auto  lg:h-[805px]">
-      <div className="icons">
+    <div className="bg-[#FFFFFF] rounded-[16px] mt-2 xl:mt-5 w-[100%] xl-w-full h-[900px] mx-auto  lg:h-[805px]">
+      
+      <div className="mobile-icons p-[20px] flex justify-between items-center lg:hidden">
+        <div className="flex w-[147px] justify-center px-2 border-gray-300 border-1 items-center cursor-pointer text-black rounded-[100px] ">
+          <SearchBlackIcon></SearchBlackIcon>
+          <Input className="border-none" placeholder="search..." />
+        </div>
 
-        <div className="flex w-[250px] justify-center px-2 border-gray-300 border-1 items-center cursor-pointer text-black rounded-[100px] ">
+        <div className="flex gap-[10px]">
+          <div className="filter border-1 border-gray-300 flex items-center cursor-pointer justify-center rounded-full w-[42px] h-[42px] lg:hidden">
+            <FilterIcon></FilterIcon>
+          </div>
+          <div className="export border-1 border-gray-300 flex items-center cursor-pointer justify-center rounded-full w-[42px] h-[42px] lg:hidden">
+            <ExportIcon></ExportIcon>
+          </div>
+        </div>
+      </div>
+
+     <div className="dekstop-icons hidden lg:flex">
+     <div className="flex w-[250px] justify-center px-2 border-gray-300 border-1 items-center cursor-pointer text-black rounded-[100px] ">
           <SearchBlackIcon></SearchBlackIcon>
           <Input className="border-none" placeholder="search anything here" />
         </div>
-      </div>
+
+     </div>
 
       <Table>
         <TableCaption>A list of your recent invoices.</TableCaption>
